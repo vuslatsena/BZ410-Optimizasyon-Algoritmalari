51f6146092f901f9e523a368aaae05c2536c33ea
VuslatSena <vuslatsena23@gmail.com>
2021-04-10 00:49:36 +0300

    Isıl İşlem Algoritması eklendi

---
+++ IsılİslemAlgoritmasi/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IsılİslemAlgoritmasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rn = new Random();
+            int dim = Convert.ToInt32(textBox1.Text);
+            double lb = Convert.ToDouble(textBox2.Text);
+            double ub = Convert.ToDouble(textBox3.Text);
+            double t = Convert.ToDouble(textBox4.Text);
+            double alpha = Convert.ToDouble(textBox5.Text);
+            int maxIter = Convert.ToInt32(textBox6.Text);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ IsılİslemAlgoritmasi/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsılİslemAlgoritmasi
+{
+   public class Solution:ICloneable
+    {
+        public double [] solutionString;
+        public double objVal = 0;
+
+        public Solution(int dim)
+        {
+            solutionString = new double[dim];
+        }
+    }
+}
